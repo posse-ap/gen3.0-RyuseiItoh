ad0416759de7ce35897798a1c9cba9e9f62f2c2c
Ryusei Ito <bsry22362@g.nihon-u.ac.jp>
2022-11-27 01:28:24 +0900

    commit

---
+++ js/script.js
@@ -0,0 +1,460 @@
+// 'use strict'
+
+
+// let buttonAnswer1 = document.getElementById('quizSelectAnswer1-1');
+
+//     buttonAnswer1.addEventListener('click', () => {
+//             let answerbox = document.getElementById('answerbox');
+//             answerbox.innerHTML = `正解!`;
+//             answerbox.classList.add('answerbox');
+
+//             let first = document.createElement('p');
+//             first.setAttribute('id', 'first');
+//             first.innerHTML = `<p>A 約28万人</p>`;
+//             answerbox.insertBefore(first, null);
+//             first.style.color = 'black';
+
+//             buttonAnswer1.style.borderColor = '#0071BC';
+//             buttonAnswer1.style.borderStyle = 'solid';
+//             buttonAnswer1.style.borderWidth = '5px';
+//             buttonAnswer1.style.borderRadius = '8px';
+//             buttonAnswer1.style.color = '#0071BC';
+
+//             buttonAnswer1.style.pointerEvents = "none";
+//             buttonDiffer1.style.pointerEvents = 'none';
+//             buttonDiffer12.style.pointerEvents = 'none';
+//         });
+
+
+// let buttonDiffer1 = document.getElementById('quizSelectDiffer1-2');
+    
+//     buttonDiffer1.addEventListener('click', () => {
+//             let differbox = document.getElementById('differbox');
+//             differbox.innerHTML = `不正解...`;
+//             differbox.classList.add('differbox');
+
+//             let second = document.createElement('p');
+//             second.setAttribute('id', 'second');
+//             second.innerHTML = `<p>A 約28万人</p>`;
+//             differbox.insertBefore(second, null);
+//             second.style.color = 'black';
+
+//             buttonDiffer1.style.borderColor = '#0071BC';
+//             buttonDiffer1.style.borderStyle = 'solid';
+//             buttonDiffer1.style.borderWidth = '5px';
+//             buttonDiffer1.style.borderRadius = '8px';
+//             buttonDiffer1.style.color = '#0071BC';
+
+//             buttonAnswer1.style.pointerEvents = "none";
+//             buttonDiffer1.style.pointerEvents = 'none';
+//             buttonDiffer12.style.pointerEvents = 'none';
+//         });
+
+// let buttonDiffer12 = document.getElementById('quizSelectDiffer1-3');
+    
+//     buttonDiffer12.addEventListener('click', () => {
+//             let differbox2 = document.getElementById('differbox');
+//             differbox2.innerHTML = `不正解...`;
+//             differbox2.classList.add('differbox');
+
+//             let second2 = document.createElement('p');
+//             second2.setAttribute('id', 'second');
+//             second2.innerHTML = `<p>A 約28万人</p>`;
+//             differbox2.insertBefore(second2, null);
+//             second2.style.color = 'black';
+
+//             buttonDiffer12.style.borderColor = '#0071BC';
+//             buttonDiffer12.style.borderStyle = 'solid';
+//             buttonDiffer12.style.borderWidth = '5px';
+//             buttonDiffer12.style.borderRadius = '8px';
+//             buttonDiffer12.style.color = '#0071BC';
+            
+//             buttonAnswer1.style.pointerEvents = "none";
+//             buttonDiffer1.style.pointerEvents = 'none';
+//             buttonDiffer12.style.pointerEvents = 'none';
+//         });
+
+
+// let buttonAnswer2 = document.getElementById('quizSelectAnswer2-1');
+
+//     buttonAnswer2.addEventListener('click', () => {
+//             let answerbox2 = document.getElementById('answerbox2');
+//             answerbox2.innerHTML = `正解!`;
+//             answerbox2.classList.add('answerbox');
+
+//             let first2 = document.createElement('p');
+//             first2.setAttribute('id', 'first');
+//             first2.innerHTML = `<p>A. INTECH</p>`;
+//             answerbox2.insertBefore(first2, null);
+//             first2.style.color = 'black';
+
+//             buttonAnswer2.style.borderColor = '#0071BC';
+//             buttonAnswer2.style.borderStyle = 'solid';
+//             buttonAnswer2.style.borderWidth = '5px';
+//             buttonAnswer2.style.borderRadius = '8px';
+//             buttonAnswer2.style.color = '#0071BC';
+
+//             buttonAnswer2.style.pointerEvents = "none";
+//             buttonDiffer2.style.pointerEvents = 'none';
+//             buttonDiffer22.style.pointerEvents = 'none';
+//         });
+
+
+// let buttonDiffer2 = document.getElementById('quizSelectDiffer2-2');
+    
+//     buttonDiffer2.addEventListener('click', () => {
+//             let differbox = document.getElementById('differbox2');
+//             differbox.innerHTML = `不正解...`;
+//             differbox.classList.add('differbox');
+
+//             let second = document.createElement('p');
+//             second.setAttribute('id', 'second');
+//             second.innerHTML = `<p>A. INTECH</p>`;
+//             differbox.insertBefore(second, null);
+//             second.style.color = 'black';
+
+//             buttonDiffer2.style.borderColor = '#0071BC';
+//             buttonDiffer2.style.borderStyle = 'solid';
+//             buttonDiffer2.style.borderWidth = '5px';
+//             buttonDiffer2.style.borderRadius = '8px';
+//             buttonDiffer2.style.color = '#0071BC';
+
+//             buttonAnswer.style.pointerEvents = "none";
+//             buttonDiffer2.style.pointerEvents = 'none';
+//             buttonDiffer22.style.pointerEvents = 'none';
+//         });
+
+// let buttonDiffer22 = document.getElementById('quizSelectDiffer2-3');
+    
+//     buttonDiffer22.addEventListener('click', () => {
+//             let differbox2 = document.getElementById('differbox2');
+//             differbox2.innerHTML = `不正解...`;
+//             differbox2.classList.add('differbox');
+
+//             let second2 = document.createElement('p');
+//             second2.setAttribute('id', 'second');
+//             second2.innerHTML = `<p>A. INTECH</p>`;
+//             differbox2.insertBefore(second2, null);
+//             second2.style.color = 'black';
+
+//             buttonDiffer22.style.borderColor = '#0071BC';
+//             buttonDiffer22.style.borderStyle = 'solid';
+//             buttonDiffer22.style.borderWidth = '5px';
+//             buttonDiffer22.style.borderRadius = '8px';
+//             buttonDiffer22.style.color = '#0071BC';
+            
+//             buttonAnswer2.style.pointerEvents = "none";
+//             buttonDiffer2.style.pointerEvents = 'none';
+//             buttonDiffer22.style.pointerEvents = 'none';
+//         });
+
+
+// let buttonAnswer3 = document.getElementById('quizSelectAnswer3-1');
+
+//     buttonAnswer3.addEventListener('click', () => {
+//             let answerbox3 = document.getElementById('answerbox3');
+//             answerbox3.innerHTML = `正解!`;
+//             answerbox3.classList.add('answerbox');
+
+//             let first3 = document.createElement('p');
+//             first3.setAttribute('id', 'first');
+//             first3.innerHTML = `<p>A 約28万人</p>`;
+//             answerbox3.insertBefore(first3, null);
+//             first3.style.color = 'black';
+
+//             buttonAnswer3.style.borderColor = '#0071BC';
+//             buttonAnswer3.style.borderStyle = 'solid';
+//             buttonAnswer3.style.borderWidth = '5px';
+//             buttonAnswer3.style.borderRadius = '8px';
+//             buttonAnswer3.style.color = '#0071BC';
+
+//             buttonAnswer3.style.pointerEvents = "none";
+//             buttonDiffer3.style.pointerEvents = 'none';
+//             buttonDiffer32.style.pointerEvents = 'none';
+//         });
+
+
+// let buttonDiffer3 = document.getElementById('quizSelectDiffer3-2');
+    
+//     buttonDiffer3.addEventListener('click', () => {
+//             let differbox3 = document.getElementById('differbox3');
+//             differbox3.innerHTML = `不正解...`;
+//             differbox3.classList.add('differbox');
+
+//             let second3 = document.createElement('p');
+//             second3.setAttribute('id', 'second');
+//             second3.innerHTML = `<p>A 約28万人</p>`;
+//             differbox3.insertBefore(second3, null);
+//             second3.style.color = 'black';
+
+//             buttonDiffer3.style.borderColor = '#0071BC';
+//             buttonDiffer3.style.borderStyle = 'solid';
+//             buttonDiffer3.style.borderWidth = '5px';
+//             buttonDiffer3.style.borderRadius = '8px';
+//             buttonDiffer3.style.color = '#0071BC';
+
+//             buttonAnswer3.style.pointerEvents = "none";
+//             buttonDiffer3.style.pointerEvents = 'none';
+//             buttonDiffer32.style.pointerEvents = 'none';
+//         });
+
+// let buttonDiffer32 = document.getElementById('quizSelectDiffer3-3');
+    
+//     buttonDiffer32.addEventListener('click', () => {
+//             let differbox32 = document.getElementById('differbox3');
+//             differbox32.innerHTML = `不正解...`;
+//             differbox32.classList.add('differbox');
+
+//             let second32 = document.createElement('p');
+//             second32.setAttribute('id', 'second');
+//             second32.innerHTML = `<p>A 約28万人</p>`;
+//             differbox32.insertBefore(second32, null);
+//             second32.style.color = 'black';
+
+//             buttonDiffer32.style.borderColor = '#0071BC';
+//             buttonDiffer32.style.borderStyle = 'solid';
+//             buttonDiffer32.style.borderWidth = '5px';
+//             buttonDiffer32.style.borderRadius = '8px';
+//             buttonDiffer32.style.color = '#0071BC';
+            
+//             buttonAnswer3.style.pointerEvents = "none";
+//             buttonDiffer3.style.pointerEvents = 'none';
+//             buttonDiffer32.style.pointerEvents = 'none';
+//         });
+
+
+// let buttonAnswer4 = document.getElementById('quizSelectAnswer4-1');
+
+//     buttonAnswer4.addEventListener('click', () => {
+//             let answerbox4 = document.getElementById('answerbox4');
+//             answerbox4.innerHTML = `正解!`;
+//             answerbox4.classList.add('answerbox');
+
+//             let first4 = document.createElement('p');
+//             first4.setAttribute('id', 'first');
+//             first4.innerHTML = `<p>A 約28万人</p>`;
+//             answerbox4.insertBefore(first, null);
+//             first4.style.color = 'black';
+
+//             buttonAnswer4.style.borderColor = '#0071BC';
+//             buttonAnswer4.style.borderStyle = 'solid';
+//             buttonAnswer4.style.borderWidth = '5px';
+//             buttonAnswer4.style.borderRadius = '8px';
+//             buttonAnswer4.style.color = '#0071BC';
+
+//             buttonAnswer4.style.pointerEvents = "none";
+//             buttonDiffer4.style.pointerEvents = 'none';
+//             buttonDiffer42.style.pointerEvents = 'none';
+//         });
+
+
+// let buttonDiffer4 = document.getElementById('quizSelectDiffer4-2');
+    
+//     buttonDiffer4.addEventListener('click', () => {
+//             let differbox4 = document.getElementById('differbox4');
+//             differbox4.innerHTML = `不正解...`;
+//             differbox4.classList.add('differbox');
+
+//             let second4 = document.createElement('p');
+//             second4.setAttribute('id', 'second');
+//             second4.innerHTML = `<p>A 約28万人</p>`;
+//             differbox4.insertBefore(second4, null);
+//             second4.style.color = 'black';
+
+//             buttonDiffer4.style.borderColor = '#0071BC';
+//             buttonDiffer4.style.borderStyle = 'solid';
+//             buttonDiffer4.style.borderWidth = '5px';
+//             buttonDiffer4.style.borderRadius = '8px';
+//             buttonDiffer4.style.color = '#0071BC';
+
+//             buttonAnswer4.style.pointerEvents = "none";
+//             buttonDiffer4.style.pointerEvents = 'none';
+//             buttonDiffer42.style.pointerEvents = 'none';
+//         });
+
+// let buttonDiffer42 = document.getElementById('quizSelectDiffer4-3');
+    
+//     buttonDiffer42.addEventListener('click', () => {
+//             let differbox42 = document.getElementById('differbox4');
+//             differbox42.innerHTML = `不正解...`;
+//             differbox42.classList.add('differbox');
+
+//             let second42 = document.createElement('p');
+//             second42.setAttribute('id', 'second');
+//             second42.innerHTML = `<p>A 約28万人</p>`;
+//             differbox42.insertBefore(second42, null);
+//             second42.style.color = 'black';
+
+//             buttonDiffer42.style.borderColor = '#0071BC';
+//             buttonDiffer42.style.borderStyle = 'solid';
+//             buttonDiffer42.style.borderWidth = '5px';
+//             buttonDiffer42.style.borderRadius = '8px';
+//             buttonDiffer42.style.color = '#0071BC';
+            
+//             buttonAnswer4.style.pointerEvents = "none";
+//             buttonDiffer4.style.pointerEvents = 'none';
+//             buttonDiffer42.style.pointerEvents = 'none';
+//         });
+
+
+// let buttonAnswer5 = document.getElementById('quizSelectAnswer5-1');
+
+//     buttonAnswer5.addEventListener('click', () => {
+//             let answerbox5 = document.getElementById('answerbox5');
+//             answerbox5.innerHTML = `正解!`;
+//             answerbox5.classList.add('answerbox');
+
+//             let first5 = document.createElement('p');
+//             first5.setAttribute('id', 'first');
+//             first5.innerHTML = `<p>A 約28万人</p>`;
+//             answerbox5.insertBefore(first5, null);
+//             first5.style.color = 'black';
+
+//             buttonAnswer5.style.borderColor = '#0071BC';
+//             buttonAnswer5.style.borderStyle = 'solid';
+//             buttonAnswer5.style.borderWidth = '5px';
+//             buttonAnswer5.style.borderRadius = '8px';
+//             buttonAnswer5.style.color = '#0071BC';
+
+//             buttonAnswer5.style.pointerEvents = "none";
+//             buttonDiffer5.style.pointerEvents = 'none';
+//             buttonDiffer52.style.pointerEvents = 'none';
+//         });
+
+
+// let buttonDiffer5 = document.getElementById('quizSelectDiffer5-2');
+    
+//     buttonDiffer5.addEventListener('click', () => {
+//             let differbox5 = document.getElementById('differbox5');
+//             differbox5.innerHTML = `不正解...`;
+//             differbox5.classList.add('differbox');
+
+//             let second5 = document.createElement('p');
+//             second5.setAttribute('id', 'second');
+//             second5.innerHTML = `<p>A 約28万人</p>`;
+//             differbox5.insertBefore(second5, null);
+//             second5.style.color = 'black';
+
+//             buttonDiffer5.style.borderColor = '#0071BC';
+//             buttonDiffer5.style.borderStyle = 'solid';
+//             buttonDiffer5.style.borderWidth = '5px';
+//             buttonDiffer5.style.borderRadius = '8px';
+//             buttonDiffer5.style.color = '#0071BC';
+
+//             buttonAnswer5.style.pointerEvents = "none";
+//             buttonDiffer5.style.pointerEvents = 'none';
+//             buttonDiffer52.style.pointerEvents = 'none';
+//         });
+
+// let buttonDiffer52 = document.getElementById('quizSelectDiffer5-3');
+    
+//     buttonDiffer52.addEventListener('click', () => {
+//             let differbox52 = document.getElementById('differbox5');
+//             differbox52.innerHTML = `不正解...`;
+//             differbox52.classList.add('differbox');
+
+//             let second52 = document.createElement('p');
+//             second52.setAttribute('id', 'second');
+//             second52.innerHTML = `<p>A 約28万人</p>`;
+//             differbox52.insertBefore(second52, null);
+//             second52.style.color = 'black';
+
+//             buttonDiffer52.style.borderColor = '#0071BC';
+//             buttonDiffer52.style.borderStyle = 'solid';
+//             buttonDiffer52.style.borderWidth = '5px';
+//             buttonDiffer52.style.borderRadius = '8px';
+//             buttonDiffer52.style.color = '#0071BC';
+            
+//             buttonAnswer5.style.pointerEvents = "none";
+//             buttonDiffer5.style.pointerEvents = 'none';
+//             buttonDiffer52.style.pointerEvents = 'none';
+//         });
+
+
+// let buttonAnswer6 = document.getElementById('quizSelectAnswer6-1');
+
+//     buttonAnswer6.addEventListener('click', () => {
+//             let answerbox6 = document.getElementById('answerbox6');
+//             answerbox6.innerHTML = `正解!`;
+//             answerbox6.classList.add('answerbox');
+
+//             let first6 = document.createElement('p');
+//             first6.setAttribute('id', 'first');
+//             first6.innerHTML = `<p>A 約28万人</p>`;
+//             answerbox6.insertBefore(first6, null);
+//             first6.style.color = 'black';
+
+//             buttonAnswer6.style.borderColor = '#0071BC';
+//             buttonAnswer6.style.borderStyle = 'solid';
+//             buttonAnswer6.style.borderWidth = '5px';
+//             buttonAnswer6.style.borderRadius = '8px';
+//             buttonAnswer6.style.color = '#0071BC';
+
+//             buttonAnswer6.style.pointerEvents = "none";
+//             buttonDiffer6.style.pointerEvents = 'none';
+//             buttonDiffer62.style.pointerEvents = 'none';
+//         });
+
+
+// let buttonDiffer6 = document.getElementById('quizSelectDiffer6-2');
+    
+//     buttonDiffer6.addEventListener('click', () => {
+//             let differbox6 = document.getElementById('differbox6');
+//             differbox6.innerHTML = `不正解...`;
+//             differbox6.classList.add('differbox');
+
+//             let second6 = document.createElement('p');
+//             second6.setAttribute('id', 'second');
+//             second6.innerHTML = `<p>A 約28万人</p>`;
+//             differbox6.insertBefore(second6, null);
+//             second6.style.color = 'black';
+
+//             buttonDiffer6.style.borderColor = '#0071BC';
+//             buttonDiffer6.style.borderStyle = 'solid';
+//             buttonDiffer6.style.borderWidth = '5px';
+//             buttonDiffer6.style.borderRadius = '8px';
+//             buttonDiffer6.style.color = '#0071BC';
+
+//             buttonAnswer6.style.pointerEvents = "none";
+//             buttonDiffer6.style.pointerEvents = 'none';
+//             buttonDiffer62.style.pointerEvents = 'none';
+//         });
+
+// let buttonDiffer62 = document.getElementById('quizSelectDiffer6-3');
+    
+//     buttonDiffer62.addEventListener('click', () => {
+//             let differbox62 = document.getElementById('differbox6');
+//             differbox62.innerHTML = `不正解...`;
+//             differbox62.classList.add('differbox');
+
+//             let second62 = document.createElement('p');
+//             second62.setAttribute('id', 'second');
+//             second62.innerHTML = `<p>A 約28万人</p>`;
+//             differbox62.insertBefore(second62, null);
+//             second62.style.color = 'black';
+
+//             buttonDiffer62.style.borderColor = '#0071BC';
+//             buttonDiffer62.style.borderStyle = 'solid';
+//             buttonDiffer62.style.borderWidth = '5px';
+//             buttonDiffer62.style.borderRadius = '8px';
+//             buttonDiffer62.style.color = '#0071BC';
+            
+//             buttonAnswer6.style.pointerEvents = "none";
+//             buttonDiffer6.style.pointerEvents = 'none';
+//             buttonDiffer62.style.pointerEvents = 'none';
+//         });
+
+
+
+
+
+// // let acJapan = '<li id ="quizSelectAnswer" class="selectbox"><button>約28万人</button></li><li id = "quizSelectDiffer" class="selectbox"><button >約78万人</button></li><li id = "quizSelectDiffer2" class="selectbox"><button >約183万人</button>';
+
+// // document.querySelector('.contain').innerHTML = acJapan;
+
+
+
+
+
+
+
